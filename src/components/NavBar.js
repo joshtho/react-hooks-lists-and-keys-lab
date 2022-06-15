2d7036f06a087d4aa9712015f75edc6e6373db2c
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-06-15 15:04:05 -0500

    commit tree c92739cd16d948243e46ff1e8da6eda2f30cdb52

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  let linksArr = links.map((link) => {
+    return <a key={link} href={`#${link}`}>{link}</a>
+  })
+  //console.log(linksArr)
+  return <nav>{linksArr}</nav>;
 }
 
 export default NavBar;
